4b9b945e820fd5f38b957faad2b942fdf6f596f8
sujon3537 <119605414+sujon3537@users.noreply.github.com>
2023-11-08 00:29:23 +0600

    emailOtp

--- controllers/emailVerifyOtpMatch.js
+++ controllers/emailVerifyOtpMatch.js
@@ -1,23 +1,22 @@
 const User = require("../models/userModel.js");
 
 const emailVerificationOtpMatch = async (req, res) => {
-    const {email, randomOtp} = req.body;
+  const { email, randomOtp } = req.body;
 
-    let findOtp = await User.find({email})
+  let findOtp = await User.find({ email });
 
-    if(findOtp.length > 0){
-        if(randomOtp == findOtp[0].randomOtp){
-            res.json({"Succcess" : "Otp matched"})
-            let removeOtpMatched = await User.findOneAndUpdate(
-                {email},
-                {$unset: {randomOtp: ""}},
-                {new: true}
-            )
-        }
-        else{
-            res.json({"error" : "Otp doesn't matched!"})
-        }
+  if (findOtp.length > 0) {
+    if (randomOtp == findOtp[0].randomOtp) {
+      res.json({ success: "Otp matched" });
+      let removeOtpMatched = await User.findOneAndUpdate(
+        { email },
+        { $unset: { randomOtp: "" } },
+        { new: true }
+      );
+    } else {
+      res.json({ error: "Otp doesn't matched!" });
     }
-}
+  }
+};
 
 module.exports = emailVerificationOtpMatch;
